14a7162953628fdc3da402d2d18c0f9b56fe4be3
Aoi <naztya2001@bk.ru>
2023-02-25 21:12:17 +0300

    Task01 is solved

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (secondNumber*secondNumber == firstNumber) {
+    Console.WriteLine("Second number squared equals first number");
+} else {
+    Console.WriteLine("Connection between these numbers is not found");
+};
