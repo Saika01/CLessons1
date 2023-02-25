1fd18ab56e6dea1134b9fe2fcbbcca130dff850e
Aoi <naztya2001@bk.ru>
2023-02-25 21:52:22 +0300

    Task05 done

---
+++ Task05/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Write a number");
+int i = Convert.ToInt32(Console.ReadLine());
+int number=i;
+if (i>0){
+    while (i>0) {
+        Console.Write(i*-1);
+        i=i-1;
+    };
+    while (number>i) {
+        Console.Write(i);
+        i=i+1;
+    };
+};
