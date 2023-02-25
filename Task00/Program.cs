290606cf11201b1765dffecf8c8c7e75b8cdd12c
Aoi <naztya2001@bk.ru>
2023-02-25 21:02:52 +0300

    00 task is done

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = Convert.ToInt32(Console.ReadLine());
+number = number*number;
+Console.WriteLine($"if squared equals {number}");
