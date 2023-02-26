d20fddafe8e32d43560ee08045525d058db4b934
Aoi <naztya2001@bk.ru>
2023-02-26 09:41:39 +0300

    Task02 done

---
+++ Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber>secondNumber){
+    Console.WriteLine("First number is bigger");
+} else if (secondNumber>firstNumber){
+    Console.WriteLine("Second number is bigger");
+} else {
+    Console.WriteLine("There is no number that is bigger than anothe one");
+};
