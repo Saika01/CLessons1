fa7709715b155b03c4e2b55c1782db9e20154445
Aoi <naztya2001@bk.ru>
2023-02-26 09:51:10 +0300

    Task04 is done

---
+++ Task04/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[3];
+Console.WriteLine("Input first number");
+arr[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+arr[1] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number");
+arr[2] = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int max = 0;
+while (count<arr.Length){
+    if (arr[count]>max){
+        max = arr[count];
+    }
+    count++;
+};
+Console.WriteLine(max + " is the maximum number");
